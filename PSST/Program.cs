b8e38c97e9171d7473cdc97b0bba17d930d72465
sagepaden <sagepaden@gmail.com>
2023-08-11 11:23:42 -0700

    BUGFIX: had a bug with vs code crashing, seems to be working now

---
+++ PSST/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PSST.Models;
+
+namespace PSST
+{
+    class Program
+    {
+        
+    }
+}
