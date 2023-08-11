301797089b43a2e8e1a3a6d0ad1fe0bceb4d8214
sagepaden <sagepaden@gmail.com>
2023-08-11 13:21:45 -0700

    slowly typing everything out for more muscle memory

--- PSST/Program.cs
+++ PSST/Program.cs
@@ -8,6 +8,53 @@ namespace PSST
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<PSSTContext>(
+                            dbContextOptions => dbContextOptions ContextOptions
+                            .UseMySql(
+                                builder.Configuration
+                                ["ConnectionStrings:DefaultConnection"],
+                                ServerVersion.AutoDetect(builder.Configuration
+                                ["ConnectionStrings:DefaultConnection"]
+                                )
+                            )
+                        );
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                            .AddEntityFrameworkStores<PSSTContext>()
+                            .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLength = 0;
+                options.Password.RequireUniqueChars = 0;
+            });
+
+            WebApplication app = builder.Build();
+
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
     }
 }
