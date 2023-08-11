612563330d653d96e06b8208710f15b2645374b3
sagepaden <sagepaden@gmail.com>
2023-08-11 14:21:51 -0700

    building Controllers

---
+++ PSST/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc
+using System.Threading.Tasks;
+using PSST.Models;
+using PSST.ViewModels;
+
+namespace PSST.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PSSTContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController (UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, PSSTContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser { UserName = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+    }
+}
