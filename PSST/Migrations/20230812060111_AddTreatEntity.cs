c48795c78e639d06f1730542fb2acdbc93dec835
sagepaden <sagepaden@gmail.com>
2023-08-11 23:08:22 -0700

    Migrations added successfully

---
+++ PSST/Migrations/20230812060111_AddTreatEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSST.Migrations
+{
+    public partial class AddTreatEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PSST/Migrations/20230812060312_AddTreatFlavorJoinEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSST.Migrations
+{
+    public partial class AddTreatFlavorJoinEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
