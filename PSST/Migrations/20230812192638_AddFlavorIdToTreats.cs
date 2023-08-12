5d33e335cad9c54e49d875eef67107ca06233800
sagepaden <sagepaden@gmail.com>
2023-08-12 12:36:29 -0700

    tracking down issues in TreatController

---
+++ PSST/Migrations/20230812192638_AddFlavorIdToTreats.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSST.Migrations
+{
+    public partial class AddFlavorIdToTreats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FlavorId",
+                table: "Treats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlavorId",
+                table: "Treats");
+        }
+    }
+}
