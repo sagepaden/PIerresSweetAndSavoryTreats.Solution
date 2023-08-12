52b77439ad0f94ff232274223f9b9e8f617b4f55
sagepaden <sagepaden@gmail.com>
2023-08-12 00:21:52 -0700

    WIP: database is working but am getting View errors. Will work more tomorrow

--- PSST/Models/PSSTContext.cs
+++ PSST/Models/PSSTContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace PSST.Models
@@ -9,5 +10,10 @@ namespace PSST.Models
         public DbSet<Flavor> Flavors { get; set; }
         public DbSet<TreatFlavor> TreatFlavors { get; set; }
         public PSSTContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.NavigationBaseIncludeIgnored));
+        }
     }
 }
